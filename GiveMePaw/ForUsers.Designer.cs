58c409fc4e3fcc95df07560e5f35f36162e355a7
FroStyRo <rmnvstrkv@gmail.com>
2023-11-10 16:57:15 +0500

    Промежуточный коммит

---
+++ GiveMePaw/ForUsers.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace GiveMePaw
+{
+    partial class ForUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForUsers));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.about_Tab_button = new System.Windows.Forms.Panel();
+            this.info_Tab_button = new System.Windows.Forms.Panel();
+            this.give_away_Tab_button = new System.Windows.Forms.Panel();
+            this.main_Tab_button = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1024, 652);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(65)))), ((int)(((byte)(48)))));
+            this.panel2.Controls.Add(this.about_Tab_button);
+            this.panel2.Controls.Add(this.info_Tab_button);
+            this.panel2.Controls.Add(this.give_away_Tab_button);
+            this.panel2.Controls.Add(this.main_Tab_button);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(239, 652);
+            this.panel2.TabIndex = 4;
+            // 
+            // about_Tab_button
+            // 
+            this.about_Tab_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(65)))), ((int)(((byte)(48)))));
+            this.about_Tab_button.Location = new System.Drawing.Point(0, 352);
+            this.about_Tab_button.Name = "about_Tab_button";
+            this.about_Tab_button.Size = new System.Drawing.Size(239, 60);
+            this.about_Tab_button.TabIndex = 7;
+            // 
+            // info_Tab_button
+            // 
+            this.info_Tab_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(65)))), ((int)(((byte)(48)))));
+            this.info_Tab_button.Location = new System.Drawing.Point(0, 286);
+            this.info_Tab_button.Name = "info_Tab_button";
+            this.info_Tab_button.Size = new System.Drawing.Size(239, 60);
+            this.info_Tab_button.TabIndex = 6;
+            // 
+            // give_away_Tab_button
+            // 
+            this.give_away_Tab_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(65)))), ((int)(((byte)(48)))));
+            this.give_away_Tab_button.Location = new System.Drawing.Point(0, 220);
+            this.give_away_Tab_button.Name = "give_away_Tab_button";
+            this.give_away_Tab_button.Size = new System.Drawing.Size(239, 60);
+            this.give_away_Tab_button.TabIndex = 6;
+            // 
+            // main_Tab_button
+            // 
+            this.main_Tab_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(65)))), ((int)(((byte)(48)))));
+            this.main_Tab_button.Location = new System.Drawing.Point(0, 154);
+            this.main_Tab_button.Name = "main_Tab_button";
+            this.main_Tab_button.Size = new System.Drawing.Size(239, 60);
+            this.main_Tab_button.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(181)))), ((int)(((byte)(152)))));
+            this.label1.Location = new System.Drawing.Point(89, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Дай лапу";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GiveMePaw.Properties.Resources.logo_give_me_paw_2;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(65)))), ((int)(((byte)(48)))));
+            this.label6.Location = new System.Drawing.Point(532, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(180, 38);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "ГЛАВНАЯ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(420, 217);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(107, 74);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ForUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 613);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1024, 652);
+            this.MinimumSize = new System.Drawing.Size(1024, 652);
+            this.Name = "ForUsers";
+            this.Text = "Дай лапу";
+            this.Load += new System.EventHandler(this.ForUsers_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel about_Tab_button;
+        private System.Windows.Forms.Panel info_Tab_button;
+        private System.Windows.Forms.Panel give_away_Tab_button;
+        private System.Windows.Forms.Panel main_Tab_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ GiveMePaw/SignIn.cs
@@ -0,0 +1,237 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiveMePaw
+{
+    public partial class SignIn : Form
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void link_to_sign_up_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            SignInDisplay.Visible = false;
+            SignUpDisplay.Visible = true;
+
+            email_sign_in.Text = "email";
+            password_sign_in.Text = "пароль";
+
+        }
+
+        private void link_to_sign_in_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            SignInDisplay.Visible = true;
+            SignUpDisplay.Visible = false;
+
+            surname_sign_up.Text = "фамилия";
+            name_sign_up.Text = "имя";
+            last_name_sign_up.Text = "отчество";
+            phone_sign_up.Text = "телефон";
+            email_sign_up.Text = "email";
+            password_sign_up.Text = "пароль";
+        }
+
+        private void save_button_sign_up_Click(object sender, EventArgs e)
+        {
+            surname_sign_up.Text = "фамилия";
+            name_sign_up.Text = "имя";
+            last_name_sign_up.Text = "отчество";
+            phone_sign_up.Text = "телефон";
+            email_sign_up.Text = "email";
+            password_sign_up.Text = "пароль";
+
+
+            SignIn.ActiveForm.Hide();
+            ForUsers NewForm = new ForUsers();
+            NewForm.ShowDialog();
+            Close();
+        }
+
+        private void sign_in_button_Click(object sender, EventArgs e)
+        {
+            String emailUser = email_sign_in.Text;
+            String passwordUser = password_sign_in.Text;
+
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `email` = @eU AND `password` = @pU", db.getConnection());
+            command.Parameters.Add("@eU", MySqlDbType.VarChar).Value = emailUser;
+            command.Parameters.Add("@pU", MySqlDbType.VarChar).Value = passwordUser;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                email_sign_in.Text = "email";
+                password_sign_in.Text = "пароль";
+                SignIn.ActiveForm.Hide();
+                ForUsers NewForm = new ForUsers();
+                NewForm.ShowDialog();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Неправильный email или пароль!");
+            }
+                
+
+            
+            
+           
+
+            //SignIn.ActiveForm.Hide();
+            //ForUsers NewForm = new ForUsers();
+           // NewForm.ShowDialog();
+           //Close();
+        }
+
+
+        /////Текст-подсказка для формы входа и регистрации/////
+        private void email_sign_in_Enter(object sender, EventArgs e)
+        {
+           if (email_sign_in.Text == "email")
+            {
+                email_sign_in.Text = "";
+            }
+        }
+        private void email_sign_in_Leave(object sender, EventArgs e)
+        {
+            if (email_sign_in.Text == "")
+            {
+                email_sign_in.Text = "email";
+            }
+        }
+
+        private void password_sign_in_Enter(object sender, EventArgs e)
+        {
+            if (password_sign_in.Text == "пароль")
+            {
+                password_sign_in.Text = "";
+                password_sign_in.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void password_sign_in_Leave(object sender, EventArgs e)
+        {
+            if (password_sign_in.Text == "")
+            {
+                password_sign_in.Text = "пароль";
+                password_sign_in.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                password_sign_in.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void surname_sign_up_Enter(object sender, EventArgs e)
+        {
+            if (surname_sign_up.Text == "фамилия")
+            {
+                surname_sign_up.Text = "";
+            }
+        }
+
+        private void surname_sign_up_Leave(object sender, EventArgs e)
+        {
+            if (surname_sign_up.Text == "")
+            {
+                surname_sign_up.Text = "фамилия";
+            }
+        }
+
+        private void name_sign_up_Enter(object sender, EventArgs e)
+        {
+            if (name_sign_up.Text == "имя")
+            {
+                name_sign_up.Text = "";
+            }
+        }
+
+        private void name_sign_up_Leave(object sender, EventArgs e)
+        {
+            if (name_sign_up.Text == "")
+            {
+                name_sign_up.Text = "имя";
+            }
+        }
+
+        private void last_name_sign_up_Enter(object sender, EventArgs e)
+        {
+            if (last_name_sign_up.Text == "отчество")
+            {
+                last_name_sign_up.Text = "";
+            }
+        }
+
+        private void last_name_sign_up_Leave(object sender, EventArgs e)
+        {
+            if (last_name_sign_up.Text == "")
+            {
+                last_name_sign_up.Text = "отчество";
+            }
+        }
+
+        private void phone_sign_up_Enter(object sender, EventArgs e)
+        {
+            if (phone_sign_up.Text == "телефон")
+            {
+                phone_sign_up.Text = "";
+            }
+        }
+
+        private void phone_sign_up_Leave(object sender, EventArgs e)
+        {
+            if (phone_sign_up.Text == "")
+            {
+                phone_sign_up.Text = "телефон";
+            }
+        }
+
+        private void email_sign_up_Enter(object sender, EventArgs e)
+        {
+            if (email_sign_up.Text == "email")
+            {
+                email_sign_up.Text = "";
+            }
+        }
+
+        private void email_sign_up_Leave(object sender, EventArgs e)
+        {
+            if (email_sign_up.Text == "")
+            {
+                email_sign_up.Text = "email";
+            }
+        }
+
+        private void password_sign_up_Enter(object sender, EventArgs e)
+        {
+            if (password_sign_up.Text == "пароль")
+            {
+                password_sign_up.Text = "";
+            }
+        }
+
+        private void password_sign_up_Leave(object sender, EventArgs e)
+        {
+            if (password_sign_up.Text == "")
+            {
+                password_sign_up.Text = "пароль";
+            }
+        }
+        /////     /////
+    }
+}
