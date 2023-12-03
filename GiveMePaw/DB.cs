c17f72c92a017c3d07acc0003ead2816d1838a1b
PteraDa <Alexrublev2004@gmail.com>
2023-12-03 15:16:37 +0500

    Написал половину дгв с животными 36:00

--- GiveMePaw/DB.cs
+++ GiveMePaw/DB.cs
@@ -1,9 +1,11 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GiveMePaw
 {
@@ -27,5 +29,91 @@ namespace GiveMePaw
         {
             return connection;
         }
+
+        public static void AddPet(Pet std)
+        {
+            DB db = new DB();
+            string sql = "INSERT INTO `pets` VALUES ('null', '@pet_type', '@name,' '@age', '@weight', '@breed', @photo)";
+            MySqlConnection con = db.getConnection();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@pet_type", MySqlDbType.Int32).Value = std.Pet_type;
+            cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = std.Name;
+            cmd.Parameters.Add("@age", MySqlDbType.Int32).Value = std.Age;
+            cmd.Parameters.Add("@Weight", MySqlDbType.Int32).Value = std.Weight;
+            cmd.Parameters.Add("@breed", MySqlDbType.VarChar).Value = std.Breed;
+            cmd.Parameters.Add("@photo", MySqlDbType.VarChar).Value = std.Photo;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Добовление прошло успешно!", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Не удалось добавить животное!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        public static void UpdatePet(Pet std, string id)
+        {
+            DB db = new DB();
+            string sql = "UPDATE `pets` SET pet_type = @pet_type, name = @name, age = @age, weight = @weight, breed = @breed, photo = @photo WHERE id = @id";
+            MySqlConnection con = db.getConnection();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@id", MySqlDbType.Int32).Value = id;
+            cmd.Parameters.Add("@pet_type", MySqlDbType.Int32).Value = std.Pet_type;
+            cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = std.Name;
+            cmd.Parameters.Add("@age", MySqlDbType.Int32).Value = std.Age;
+            cmd.Parameters.Add("@Weight", MySqlDbType.Int32).Value = std.Weight;
+            cmd.Parameters.Add("@breed", MySqlDbType.VarChar).Value = std.Breed;
+            cmd.Parameters.Add("@photo", MySqlDbType.VarChar).Value = std.Photo;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Изменение прошло успешно!", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Не удалось изменить животное!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        public static void DeletePet(string id)
+        {
+            DB db = new DB();
+            string sql = "DELETE FROM pets WHERE id = @id";
+            MySqlConnection con = db.getConnection();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@id", MySqlDbType.Int32).Value = id;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Удаление прошло успешно!", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Не удалось удалить животное!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+        public static void DisplayAndSerachPet(String query, DataGridView dvg)
+        {
+            DB db = new DB();
+            string sql = query;
+            MySqlConnection con = db.getConnection();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            DataTable tbl = new DataTable();
+            adp.Fill(tbl);
+            con.Close();  
+
+
+        }
     }
 }
+
